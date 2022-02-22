64636dbb2bad4a0efc3b714ce67816cc8828e524
Smith Amanda Imoabasi <85838857+amandasmyth@users.noreply.github.com>
2022-02-22 13:43:18 +0100

    Add files via upload
    open source tool for automated testing of webapps across many browsers. It provides capabilities for navigating to web pages

---
+++ main.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+
+chrome_driver_path = "C:\Development\chromedriver.exe"
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
+
+driver.get("https://instagram.com")
+driver.close()
+
